95e7a6add40ab168c9767d5830d60e8758f04de4
BayuAirBender <47329758+bayuairbender2950@users.noreply.github.com>
2024-06-25 00:14:54 +0800

    Create setup.sh

---
+++ script/setup.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+echo "
+   _____      __                ________           __  _     
+  / ___/_____/ /_  ____  ____  / /_  __/___ ______/ /_(_)____
+  \__ \/ ___/ __ \/ __ \/ __ \/ / / / / __  / ___/ __/ / ___/
+ ___/ / /__/ / / / /_/ / /_/ / / / / / /_/ (__  ) /_/ / /__  
+/____/\___/_/ /_/\____/\____/_/ /_/  \__,_/____/\__/_/\___/  
+                                                                                                                                       
+  Welcome to the SchoolTastic Installation Wizard!
+"
+
+echo "[ ! ] Downloading SchoolTastic files..."
+trap 'echo "**[ ! ] Download interrupted. Exiting..." ; exit' INT
+curl -sSL https://github.com/AmbrosiaGG/SchoolTastic/archive/refs/heads/main.zip > SchoolTastic.zip
+trap - INT
+unzip SchoolTastic.zip
+rm SchoolTastic.zip
+mv SchoolTastic-main SchoolTastic
+cd SchoolTastic
+
+echo "️[ ! ] Configuring environment..."
+read -p "MongoDB URL (e.g., mongodb://admin:password@example.com:port/?authSource=admin): " atlas
+read -p "Admin Password (it'll be encrypted, so make sure to save the encrypted password): " session
+read -p "Port number (default: 3000): " port
+port=${port:-3000} 
+read -p "Auto Update (true/false, default: true): " updates
+updates=${updates:true}  
+read -p "Your School title: " title
+
+echo "atlas=$atlas" >> .env
+echo "session=$session" >> .env
+echo "port=$port" >> .env
+echo "updates=$updates" >> .env
+echo "title=$title" >> .env
+
+echo "[ ! ] Gearing up for Bun installation..."
+curl -fsSl https://bun.sh/install | bash
+source /root/.bashrc
+
+echo "[ ! ] Installing SchoolTastic dependencies..."
+bun i
+
+echo "[ ! ] Launching SchoolTastic with pm2..."
+pm2 start --interpreter ~/.bun/bin/bun index.js --name "SchoolTastic" -- -color
+sleep 5
+
+echo "[ ! ] SchoolTastic is ready! You can manage it using pm2 commands."
+echo "[ ! ] Access the webpage on http://localhost:$(sed -n '3p' .env | cut -d '=' -f2)"
+echo "[ ! ] with credentials: "
+echo "[ * ] - Username: admin"
+echo "[ * ] - Password: $(cat initialAdminPassword.txt) make sure you save this."
+echo "[ * ] - the password will be saved on ./SchoolTastic/initialAdminPassword.txt" 
+exit 0
