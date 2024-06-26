06144ce6c7b9b12f3c791d47bfe7d6801a2b4ede
DuckeyDev <duckydev@ambrosia.gg>
2024-06-26 07:59:43 +0000

    FATAL(HALF OF UI GOT CUT)

--- routers/dashboard.js
+++ routers/dashboard.js
@@ -8,16 +8,17 @@ router.get("/", async (req, res) => {
   if (!req.session.username) {
     console.log(req.session.name)
     res.redirect('/login?description=Not%20Logged%20In&title=Error&Icon=error ')
-  } 
-      res.render("../views/dashboard/index.ejs", {
-        title: process.env.title,
-        req: req,
-        res: res,
-        // 6/24/2024 8:48AM | I should find a Better way to do this
-        role: req.session.role,
-        name: req.session.username,
-        pass: req.session.not_listd,
-      });
+  } else {
+    res.render("../views/dashboard/index.ejs", {
+      title: process.env.title,
+      req: req,
+      res: res,
+      // 6/24/2024 8:48AM | I should find a Better way to do this
+      role: req.session.role,
+      name: req.session.username,
+      pass: req.session.not_listd,
+    });
+  }
 });
 
 module.exports = router;
